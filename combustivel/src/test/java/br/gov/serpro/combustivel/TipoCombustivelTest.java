b5a9d9f76c6eb7cf9ca890d9905cfabbbd736ce2
aluno <aluno@10.32.18.34>
2019-04-15 15:59:47 -0300

    Versionamento versão inicial do primeiro teste

--- combustivel/src/test/java/br/gov/serpro/combustivel/TipoCombustivelTest.java
+++ combustivel/src/test/java/br/gov/serpro/combustivel/TipoCombustivelTest.java
@@ -1,14 +1,19 @@
 package br.gov.serpro.combustivel;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
+
+import junit.*;;
 
 public class TipoCombustivelTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSeDeveAbastecerComEtanol() {
+		PrecoCombustivel precoEtanol = new PrecoCombustivel("Etanol");
+		PrecoCombustivel precoGasolina = new PrecoCombustivel("Gasolina");
+		
+		Assert.assertTrue((precoEtanol/precoGasolina)<=0.7);
+		
 	}
 
 }
