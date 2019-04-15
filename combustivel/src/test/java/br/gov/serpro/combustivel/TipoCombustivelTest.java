6d1fecce183cf9d09cf00a13be232ae3e90e65c6
aluno <aluno@10.32.18.34>
2019-04-15 15:49:29 -0300

    Versionamento inicial do projeto

---
+++ combustivel/src/test/java/br/gov/serpro/combustivel/TipoCombustivelTest.java
@@ -0,0 +1,14 @@
+package br.gov.serpro.combustivel;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TipoCombustivelTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
